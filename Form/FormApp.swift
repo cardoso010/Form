cc225ecc827a34ba20c0ee446f5e7e3396b23f77
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-26 12:39:32 -0300

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Gabriel Cardoso on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
